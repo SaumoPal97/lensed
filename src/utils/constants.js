2833af7d0038e5af94be1afdbdcd15842c022a26
SaumoPal97 <saumopal1997@gmail.com>
2022-08-10 13:37:36 +0530

    setting base lens context

---
+++ src/utils/constants.js
@@ -0,0 +1,9 @@
+import lenshubabi from "./lenshub.json";
+import lenshubperipheryabi from "./lenshubperiphery.json";
+
+export const LENS_HUB_CONTRACT_ADDRESS =
+  "0xDb46d1Dc155634FbC732f92E853b10B288AD5a1d";
+export const PERIPHERY_CONTRACT_ADDRESS =
+  "0xeff187b4190E551FC25a7fA4dFC6cf7fDeF7194f";
+export const LENS_HUB_CONTRACT_ABI = lenshubabi;
+export const PERIPHERY_CONTRACT_ABI = lenshubperipheryabi;
